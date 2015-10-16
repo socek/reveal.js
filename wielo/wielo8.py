733d9850517c43dec3db96d440503aa6a4597fd2
Dominik "Socek" Dlugajczyk <msocek@gmail.com>
2015-10-16 17:03:13 +0200

    After

--- wielo/wielo8.py
+++ wielo/wielo8.py
@@ -1,30 +1,10 @@
-class FirstMeta(type):
-    pass
+class FirstMeta(type): pass
+class SecondMeta(type): pass
 
+class First(object, metaclass=FirstMeta): pass
+class Second(object, metaclass=SecondMeta): pass
 
-class SecondMeta(type):
-    pass
+class ThirdMeta(FirstMeta, SecondMeta): pass
 
+class Third(First, Second, metaclass=ThirdMeta): pass
 
-class First(object, metaclass=FirstMeta):
-    pass
-
-
-class Second(object, metaclass=SecondMeta):
-    pass
-
-
-class FirstSecond(First):
-    pass
-
-
-class ThirdMeta(FirstMeta, SecondMeta):
-    pass
-
-
-class Third(First, Second, metaclass=ThirdMeta):
-    pass
-
-
-class Four(Third, FirstSecond):
-    pass
